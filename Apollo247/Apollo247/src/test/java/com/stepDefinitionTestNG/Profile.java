7baeab7c77ea2155c780fb81f41e63a28912dbaa
Koyyada-Anvitha <koyyada-anvitha.koyyada-anvitha@capgemini.com>
2025-11-13 23:33:17 +0530

    3rd commit Automation

--- Apollo247/Apollo247/src/test/java/com/stepDefinitionTestNG/Profile.java
+++ Apollo247/Apollo247/src/test/java/com/stepDefinitionTestNG/Profile.java
@@ -1,4 +1,5 @@
 package com.stepDefinitionTestNG;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
@@ -8,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import com.pages.CircleMembershipPage;
+import com.parameters.ExcelReader;
 import com.parameters.PropertyReader;
 import com.setup.BaseSteps;
 
@@ -50,8 +52,8 @@ import io.cucumber.java.en.When;
 		//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		//++++++++++++++++++++++++++++++ second scenario ++++++++++++++++++++++++++++++
 		
-		@Given("the user on Apollo247 homepage")
-		public void the_user_on_apollo247_homepage() 
+		@Given("the user on Apollo247 homepage2")
+		public void the_user_on_apollo247_homepage2() 
 		{
 		    // Write code here that turns the phrase above into concrete actions
 		    // throw new io.cucumber.java.PendingException();
@@ -108,99 +110,183 @@ import io.cucumber.java.en.When;
 	   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	   //++++++++++++++++++++++++++++++ 3rd scenario ++++++++++++++++++++++++++++++++++++++++++++++
 		
-		@Given("the user is on the Apollo247 homepage")
-		public void the_user_is_on_the_apollo247_homepage() {
+		
+//		String num;
+//		int rowIndex = 0;
+//		@Given("the user is on the Apollo247 homepage")
+//		public void the_user_is_on_the_apollo247_homepage() {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    //throw new io.cucumber.java.PendingException();
+//			BaseSteps.launchBrowser();
+//	        circlePage = new CircleMembershipPage(BaseSteps.driver);
+//	        BaseSteps.driver.get(PropertyReader.readProperty().getProperty("sourceUrl"));
+//			
+//		}
+//
+//		@When("the user clicked on Circle Membership")
+//		public void the_user_clicked_on_circle_membership() {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    // throw new io.cucumber.java.PendingException();
+//			circlePage.clickCircleMembership();
+//			
+//		}
+//
+//		@When("the user clicked on Join Circle")
+//		public void the_user_clicked_on_join_circle() {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    // throw new io.cucumber.java.PendingException();
+//			circlePage.clickJoinCircle();
+//			
+//		}
+//
+//		@When("the user clicks on Login to Continue")
+//		public void the_user_clicks_on_login_to_continue() 
+//		{
+//		    circlePage.clickLoginToContinue();
+//
+//		}
+//		
+//		@When("the user clicks on search mobile number")
+//		public void the_user_clicks_on_search_mobile_number() throws IOException {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    // throw new io.cucumber.java.PendingException();
+//			 circlePage.clickAndEnterPhoneNumber(0, 0);
+//		}
+		
+		//-----------------------------------------------------------------------------------------------------
+		
+//		@When("the user enters phone number from sheet {int} and row {int}") 
+//		public void the_user_enters_phone_number_from_sheet_and_row(Integer int1, Integer int2) throws IOException {
+//		    //Write code here that turns the phrase above into concrete actions
+//		    //throw new io.cucumber.java.PendingException();
+//			Properties prop = PropertyReader.readProperty();
+//			String excelPath = prop.getProperty("number");
+//			num=ExcelReader.getCellData(num, 0, rowIndex);
+//			circlePage.clickAndEnterPhoneNumber(0, 0);
+//			
+//		}
+		
+//		@When("the user enters phone number from sheet {int} and row {int}")
+//		public void the_user_enters_phone_number_from_sheet_and_row(Integer sheetIndex, Integer rowIndex) throws IOException {
+//		    // Get Excel file path from Profile.properties
+//		    Properties prop = PropertyReader.readProperty();
+//		    String excelPath = prop.getProperty("number"); // This is your file path
+//
+//		    // Read phone number from Excel
+//		    String phone = ExcelReader.getCellData(excelPath, sheetIndex, rowIndex, 0); // column 0 assumed
+//
+//		    // Pass phone number to page method
+//		    //circlePage.enterPhoneNumber(phone);
+//		}
+		//----------------------------------------------------------------------------------------------------------
+		
+//		@When("the user enters phone number from {int}")
+//		public void the_user_enters_phone_number_from(Integer int1) throws IOException 
+//		{
+//		    // Write code here that turns the phrase above into concrete actions
+//		   // throw new io.cucumber.java.PendingException();
+//			Properties prop = PropertyReader.readProperty();
+//		    String excelPath = prop.getProperty("number");
+//		    String phone = ExcelReader.getCellData(excelPath, int1, rowIndex);
+//		}
+//
+//		@When("the user clicks on Continue")
+//		public void the_user_clicks_on_continue() {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    //throw new io.cucumber.java.PendingException();
+//		}
+//
+//		@When("wait for OTP entry")
+//		public void wait_for_otp_entry() {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    //throw new io.cucumber.java.PendingException();
+//		}
+//
+//		@When("the user waits for OTP and clicks verify")
+//		public void the_user_waits_for_otp_and_clicks_verify() {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    // throw new io.cucumber.java.PendingException();
+//		}
+//
+//		@Then("user should be logged in Succesfully")
+//		public void user_should_be_logged_in_succesfully() {
+//		    // Write code here that turns the phrase above into concrete actions
+//		    //throw new io.cucumber.java.PendingException();
+//		}
+		
+		//++++++++++++++++++++++++++++++++++ 4th Scenario ++++++++++++++++++++++++++++++++++++++++++++++
+		
+		@Given("the user is on the Apollo247 homepage3")
+		public void the_user_is_on_the_apollo247_homepage3() {
 		    // Write code here that turns the phrase above into concrete actions
-		   // throw new io.cucumber.java.PendingException();
+		    //throw new io.cucumber.java.PendingException();
 			BaseSteps.launchBrowser();
 	        circlePage = new CircleMembershipPage(BaseSteps.driver);
 	        BaseSteps.driver.get(PropertyReader.readProperty().getProperty("sourceUrl"));
 			
 		}
 
-		@When("the user clicked on Circle Membership")
-		public void the_user_clicked_on_circle_membership() {
+		@When("the user clicked on the Circle Membership")
+		public void the_user_clicked_on_the_circle_membership() {
 		    // Write code here that turns the phrase above into concrete actions
-		    //throw new io.cucumber.java.PendingException();
+		   // throw new io.cucumber.java.PendingException();
 			circlePage.clickCircleMembership();
 			
 		}
 
-		@When("the user clicked on Join Circle")
-		public void the_user_clicked_on_join_circle() {
+		@When("the user clicks on the Join Circle")
+		public void the_user_clicks_on_the_join_circle() {
 		    // Write code here that turns the phrase above into concrete actions
 		    //throw new io.cucumber.java.PendingException();
 			circlePage.clickJoinCircle();
 		}
 
-		@When("join circle page is visible")
-		public void join_circle_page_is_visible() {
-		    // Write code here that turns the phrase above into concrete actions
-		    //throw new io.cucumber.java.PendingException();
-		}
-
-		@When("the user clicks on Login to Continue")
-		public void the_user_clicks_on_login_to_continue() {
-		    // Write code here that turns the phrase above into concrete actions
-		    // throw new io.cucumber.java.PendingException();
-
-//				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//       			WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
-//       			By.xpath("//button[contains(text(),'Login to Continue')]")));
-//                loginButton.click();
-			circlePage.clickLoginToContinue();
-
-		}
-
-		Properties props = new Properties();
 		
-
-		@When("the user enters phone number")
-		public void the_user_enters_phone_number() {
-		    // Write code here that turns the phrase above into concrete actions
-		    // throw new io.cucumber.java.PendingException();
-
-			 String phoneNumber = props.getProperty("phone.number");
-			    circlePage.enterPhoneNumber(phoneNumber);
-
-		}
-
-		@When("the user clicks on Continue")
-		public void the_user_clicks_on_continue() {
-		    // Write code here that turns the phrase above into concrete actions
-		    // throw new io.cucumber.java.PendingException();
-			circlePage.clickContinue();
-		}
-
-		@When("wait for OTP entry")
-		public void wait_for_otp_entry() {
-		    // Write code here that turns the phrase above into concrete actions
-		    // throw new io.cucumber.java.PendingException();
-
-
-			int waitTime = Integer.parseInt(props.getProperty("otp.wait.time", "50")); // Default 50
-			circlePage.waitForOtpEntry(waitTime);
-
-		}
-
-		@When("the user clicks on Verify after entering OTP manually")
-		public void the_user_clicks_on_verify_after_entering_otp_manually() {
-		    // Write code here that turns the phrase above into concrete actions
-		    // throw new io.cucumber.java.PendingException();
-		}
-
-		@Then("user is logged in")
-		public void user_is_logged_in() {
+		
+		@When("the user clicks on the apply Coupon")
+		public void the_user_clicks_on_the_apply_coupon() {
 		    // Write code here that turns the phrase above into concrete actions
 		    //throw new io.cucumber.java.PendingException();
+			circlePage.clickApplyCoupon();
 		}
-	
-	
-	
-	
-	
-	
-	
+
+		@When("the user enters coupon code from {int}")
+		public void the_user_enters_coupon_code_from(Integer int1) {
+		    // Write code here that turns the phrase above into concrete actions
+		    // throw new io.cucumber.java.PendingException();
+
+		try {
+        // Read excel path from properties
+        String excelPath = PropertyReader.get("excelPath");
+
+        // Load Excel
+        ExcelReader.loadExcel(excelPath);
+
+        // Get coupon code from sheet "Coupons" (assuming first column)
+        String couponCode = ExcelReader.getCellData("Sheet1", int1, 0);
+
+        // Enter coupon code using Page Object
+        circlePage.enterCouponCode(couponCode);
+
+		}
+
+		catch (Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException("Failed to enter coupon code from Excel");
+		}
+
+		}		
+		
+		@Then("the user clicks on Cancel")
+		public void the_user_clicks_on_cancel() 
+		{
+		    // Write code here that turns the phrase above into concrete actions
+//		    //throw new io.cucumber.java.PendingException();
+			 circlePage.clickCancel();
+		}
+
+
+
 	
 	
 }
@@ -208,6 +294,11 @@ import io.cucumber.java.en.When;
 
 
 
+	
+	
+	
+	
+	
 
 
 
--- Apollo247/Apollo247/src/test/resources/Features/CircleMembership.feature
+++ Apollo247/Apollo247/src/test/resources/Features/CircleMembership.feature
@@ -7,7 +7,7 @@ Then the plans for you page is visible
 
 @Join
 Scenario: Navigate to Join Circle from CircleMembership
-    Given the user on Apollo247 homepage
+    Given the user on Apollo247 homepage2
     When the user clicks on the Circle Membership
     And the user clicks on Join Circle
     And join circle page is displayed
@@ -15,18 +15,34 @@ Scenario: Navigate to Join Circle from CircleMembership
     
 
 
+#@no
+#Scenario: Complete login and prepare for payment after selecting plan
+    #Given the user is on the Apollo247 homepage
+    #When the user clicked on Circle Membership
+    #And the user clicked on Join Circle
+    #And the user clicks on Login to Continue
+    #And the user clicks on search mobile number
+    #And the user enters phone number from <rowIndex>
+    #And the user clicks on Continue
+    #And wait for OTP entry
+    #And the user waits for OTP and clicks verify
+    #Then user should be logged in Succesfully
 
-Scenario: Complete login and prepare for payment after selecting plan
-    Given the user is on the Apollo247 homepage
-    When the user clicked on Circle Membership
-    And the user clicked on Join Circle
-    And join circle page is visible
-    And the user clicks on Login to Continue
-    And the user enters phone number
-    And the user clicks on Continue
-    And wait for OTP entry
-    And the user clicks on Verify after entering OTP manually
-    Then user is logged in
-
-
-
+#Examples: 
+   #| rowIndex |
+   #|  0  	   |
+ 
+  
+ @coupon
+ Scenario: Entering Coupons
+    Given the user is on the Apollo247 homepage3
+    When the user clicked on the Circle Membership
+    And the user clicks on the Join Circle
+    And the user clicks on the apply Coupon
+    And the user enters coupon code from <rowIndex>
+    Then the user clicks on Cancel
+    
+ Examples:
+    | rowIndex |
+    |    0     |
+    
